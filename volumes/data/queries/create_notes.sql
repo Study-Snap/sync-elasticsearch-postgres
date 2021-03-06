9a1b41ce059db1d3c02d6ce8f8700976d733237b
SystemFiles <bensykes12@gmail.com>
2021-03-06 16:52:30 -0500

    (checkpoint): working for create and update

--- volumes/data/queries/create_notes.sql
+++ volumes/data/queries/create_notes.sql
@@ -1,6 +1,7 @@
 CREATE TABLE notes (
+  id INTEGER PRIMARY KEY NOT NULL,
   title VARCHAR(50) NOT NULL,
   body VARCHAR NOT NULL
 );
-INSERT INTO notes (title, body)
-  VALUES ('Science 101', 'A note about everything that will and has ever had to do with science.');
+INSERT INTO notes (id, title, body)
+  VALUES (1, 'Science 101', 'A note about everything that will and has ever had to do with science.');
